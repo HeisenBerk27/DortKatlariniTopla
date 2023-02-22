8d3162f1db4b474e5f8f67788a7c46ad60dad9e6
Berk Akgül <108866525+HeisenBerk27@users.noreply.github.com>
2023-02-22 15:53:53 +0300

    Add files via upload

---
+++ src/dongu/TekSayilarinToplami.java
@@ -0,0 +1,27 @@
+package dongu;
+import java.util.Scanner;
+public class TekSayilarinToplami {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int toplam =0;
+		int sayi;
+		
+		do
+		{
+			System.out.println("Bir sayi giriniz");
+			 sayi = scanner.nextInt();
+			 
+			 if(sayi%2 ==0 && sayi%4 ==0)
+			 {
+				 toplam += sayi;
+			 }
+		}
+		while(sayi%2 ==1);
+		
+		System.out.println(toplam);
+	}
+	
+	
+}
